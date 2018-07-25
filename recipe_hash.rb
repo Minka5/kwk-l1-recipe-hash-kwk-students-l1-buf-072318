f6e54cc20f76c0450691fd61ac3f947a79101aad
Minka5 <madrakowsky5@gmail.com>
2018-07-25 20:19:41 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,6 +1,6 @@
 #code your solutions here
 
-perfect_10_recipe = {:almond_flour=>"2 cups", :gluten_free_whole_oats=>"1 cup", :kosher_salt=>"1/2 tea spoon", :baking_po, :almonds=>"1/4 cup", :mini_dark_chocolate_chips=>"3/4 cup", :olive_oil=>"1/2 cup", :agave=>"1/4 cup"}
+perfect_10_recipe = {:almond_flour=>"2 cups", :gluten_free_whole_oats=>"1 cup", :kosher_salt=>"1/2 tea spoon", :baking_powder => "1/2 tea spoon", :almonds=>"1/4 cup", :mini_dark_chocolate_chips=>"3/4 cup", :olive_oil=>"1/2 cup", :agave=>"1/4 cup"}
 
 def recipe_ingredients(perfect_10_recipe)
     perfect_10_recipe
