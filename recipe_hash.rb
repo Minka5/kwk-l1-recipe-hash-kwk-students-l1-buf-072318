a83ba58e2beba488917d566a04145f3463d57002
Minka5 <madrakowsky5@gmail.com>
2018-07-25 20:11:02 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,6 +1,6 @@
 #code your solutions here
 
-perfect_10_recipe = { :almond_flour => "2 cups",:gluten_free_whole_oats => "1 cup",
+perfect_10_recipe = { :almond_flour => "2 cups",:gluten_free_whole_oats => "1 cup",:kosher_salt => "1/2 tea spoon",
 
 
 
