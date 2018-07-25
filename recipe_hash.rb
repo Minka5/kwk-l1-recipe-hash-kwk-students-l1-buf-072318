e6216c874108b97364720a15c25e262a4f55decf
Minka5 <madrakowsky5@gmail.com>
2018-07-25 20:42:02 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -56,7 +56,7 @@ end
 
 #6: Return the number of ingredients in the `perfect_10_recipe` hash.
 
-def number_ingredients(perfect_10_recipe)
+def number_ingredients(x)
     
   perfect_10_recipe.size 
 
